4b58c744e14ecf3c9a0cf1317ce125ee819b7af1
h-takeyeah <61489178+h-takeyeah@users.noreply.github.com>
2024-09-06 01:49:42 +0900

    enter deep-sleep on setup

--- arduino_slack_status_updater.ino
+++ arduino_slack_status_updater.ino
@@ -6,10 +6,36 @@
 
 #include "secrets.h"
 
+RTC_DATA_ATTR int bootCount = 0;
+
+void print_wakeup_reason(){
+  esp_sleep_wakeup_cause_t wakeup_reason;
+
+  wakeup_reason = esp_sleep_get_wakeup_cause();
+
+  switch(wakeup_reason) {
+    case ESP_SLEEP_WAKEUP_EXT0 : Serial.println("Wakeup caused by external signal using RTC_IO"); break;
+    case ESP_SLEEP_WAKEUP_EXT1 : Serial.println("Wakeup caused by external signal using RTC_CNTL"); break;
+    case ESP_SLEEP_WAKEUP_TIMER : Serial.println("Wakeup caused by timer"); break;
+    case ESP_SLEEP_WAKEUP_TOUCHPAD : Serial.println("Wakeup caused by touchpad"); break;
+    case ESP_SLEEP_WAKEUP_ULP : Serial.println("Wakeup caused by ULP program"); break;
+    default : Serial.printf("Wakeup was not caused by deep sleep: %d\n", wakeup_reason); break;
+  }
+}
+
 void setup() {
   M5.begin();
   M5.Power.begin();
   Serial.begin(115200);
+
+  ++bootCount;
+  Serial.println("Boot number:" + String(bootCount));
+
+  print_wakeup_reason();
+
+  Serial.println("Going to sleep now");
+  Serial.flush();
+  M5.Power.deepSleep(0);
 }
 
 void do_post(String payload) {
@@ -35,8 +61,8 @@ void do_post(String payload) {
     {
       // Add a scoping block for HTTPClient https to make sure it is destroyed before WiFiClientSecure *client is
       HTTPClient https;
-      if (https.begin(*client, F("https://slack.com/api/users.profile.set"))) {
-        https.addHeader(F("Content-Type"), F("application/json"));
+      if (https.begin(*client, "https://slack.com/api/users.profile.set")) {
+        https.addHeader("Content-Type", "application/json");
         String auth = "Bearer ";
         auth += slack_user_token;
         https.addHeader("Authorization", auth);
