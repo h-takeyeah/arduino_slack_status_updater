eac3a18641052d6ba8e0a62adf5be3bcc439a137
h-takeyeah <61489178+h-takeyeah@users.noreply.github.com>
2024-09-05 23:34:17 +0900

    avoid using old type HTTPClient
    HTTPClient::begin without client is not recommended.
    see here <https://github.com/search?q=repo%3Aespressif%2Farduino-esp32+HTTPCLIENT_1_1_COMPATIBLE&type=issues#issuecomment-551051715>
    
    Also, M5Stack example (BasicHTTPSClient.ino) uses newer API.

--- arduino_slack_status_updater.ino
+++ arduino_slack_status_updater.ino
@@ -2,59 +2,72 @@
 #include <HTTPClient.h>
 #include <M5Stack.h>
 #include <WiFi.h>
+#include <WiFiClientSecure.h>
 
 #include "secrets.h"
-
-HTTPClient client;
 
 void setup() {
   M5.begin();
   M5.Power.begin();
+  Serial.begin(115200);
 }
 
 void do_post(String payload) {
   WiFi.begin(ssid, pass);
-  M5.Lcd.setCursor(0, 0);
-  M5.Lcd.print("connecting to Wifi");
+  Serial.print("connecting to Wifi");
   unsigned long timeout = millis();
   while (WiFi.status() != WL_CONNECTED) {
     if (millis() - timeout > 5000) {
-      M5.Lcd.println("\nfailed to connect to WiFi");
+      Serial.println("\nfailed to connect to WiFi");
       return;
     }
-    M5.Lcd.print(".");
+    Serial.print(".");
     delay(1000);
   }
 
-  M5.Lcd.printf("\nconnected to %s\n", ssid);
+  Serial.printf("\nconnected to %s\n", ssid);
 
-  M5.Lcd.println("[HTTPS] begin...");
-  client.begin(F("https://slack.com/api/users.profile.set"), root_ca);
+  Serial.println("[HTTPS] begin...");
 
-  client.addHeader(F("Content-Type"), F("application/json"));
-  String auth = "Bearer ";
-  auth += slack_user_token;
-  client.addHeader("Authorization", auth);
+  WiFiClientSecure *client = new WiFiClientSecure;
+  if (client) {
+    client->setCACert(root_ca);
+    {
+      // Add a scoping block for HTTPClient https to make sure it is destroyed before WiFiClientSecure *client is
+      HTTPClient https;
+      if (https.begin(*client, F("https://slack.com/api/users.profile.set"))) {
+        https.addHeader(F("Content-Type"), F("application/json"));
+        String auth = "Bearer ";
+        auth += slack_user_token;
+        https.addHeader("Authorization", auth);
 
-  int httpCode = client.POST(payload);
+        int httpCode = https.POST(payload);
 
-  if (httpCode > 0) {
-    M5.Lcd.printf("[HTTPS] GET... code=%d\n", httpCode);
-  } else {
-    M5.Lcd.printf("[HTTPS] GET... failed, error=%s\n", client.errorToString(httpCode).c_str());
+        if (httpCode > 0) {
+          Serial.printf("[HTTPS] POST... code=%d\n", httpCode);
+        } else {
+          Serial.printf("[HTTPS] POST... failed, error=%s\n", https.errorToString(httpCode).c_str());
+        }
+
+        Serial.println("[HTTPS] end");
+        https.end();
+      } else {
+        Serial.println("[HTTPS] unable to connect");
+      }
+    }
+    delete client;
   }
-
-  M5.Lcd.println("[HTTPS] end");
-  Wifi.disconnect();
 }
 
 void loop() {
   M5.update();
-  if (M5.BtnA.wasReleased() || M5.BtnA.pressedFor(1000, 200)) {
-    do_post(F("{\"profile\":{\"status_text\":\"riding a train\",\"status_emoji\":\":train:\"}}"));
-    M5.Lcd("slack status was set");
-  } else if (M5.BtnB.wasReleased() || M5.BtnB.pressedFor(1000, 200)) {
-    do_post();
-    M5.Lcd("slack status was cleared");
+  if (M5.BtnA.wasReleased()) {
+    do_post("{\"profile\":{\"status_text\":\"riding a train\",\"status_emoji\":\":train:\"}}");
+    Serial.println("slack status was set");
+    delay(1000);
+  } else if (M5.BtnB.wasReleased()) {
+    do_post("{\"profile\":{\"status_text\":\"\",\"status_emoji\":\"\"}}");
+    Serial.println("slack status was cleared");
+    delay(1000);
   }
 }
